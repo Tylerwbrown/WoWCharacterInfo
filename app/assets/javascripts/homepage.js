2ff52cec3594404974df43fa6d42ff2446aae9a8
Tyler Brown <tyler@sliceofnet.com>
2017-06-30 07:03:16 -0400

    Non functioning code but possibly a lot cleaner

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -1,17 +1,53 @@
-var request = new XMLHttpRequest();
-request.open("GET", "https://us.api.battle.net/wow/realm/status?locale=en_US&apikey=pmqd9mhcmpek2zfdfnu975ht2rbxuwa7", false);
-request.send();
+const readyState = {
+  UNITIALIZED: 0,
+  LOADING: 1,
+  LOADED: 2,
+  INTERACTIVE: 3,
+  COMPLETED: 4
+}
 
-var json = JSON.parse(request.responseText)
-
-var realm_names = json.realms.map(function(val, i) {
-  return val['name']
-});
-$(document).ready(function(){
-  $('input.autocomplete').autocomplete({
-    source: function(request, response) {
-      var results = $.ui.autocomplete.filter(realm_names, request.term)
-      response(results.slice(0, 5))
+function handleRequest(url, callback) {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+      callback(request.responseText)
     }
+  };
+
+  request.open("GET", url, true);
+  request.send();
+}
+
+function realmStatusRequestResponse(responseJSON) {
+  realmNames = JSON.parse(responseJSON).realms.map(function(value, index) {
+    return value['name']
+  });
+
+  $(document).ready(function(){
+    $('input.autocomplete').autocomplete({
+      source: function(request, response) {
+        var results = $.ui.autocomplete.filter(realmNames, request.term)
+        response(results.slice(0, 5))
+
+        alert(realmNames)
+      }
+    });
+  });
+
+}
+
+function characterRequestResponse(responseJSON) {
+  alert(responseJSON['level'])
+}
+realmStatusURL = "https://us.api.battle.net/wow/realm/status?locale=en_US&apikey=pmqd9mhcmpek2zfdfnu975ht2rbxuwa7"
+handleRequest(realmStatusURL, realmStatusRequestResponse)
+
+$(function(){
+  $("#search-for-character").click(function() {
+    var realm = $("input#realm-name").val()
+    var characterName = $("input#character-name").val()
+
+    var characterRequestURL = "https://us.api.battle.net/wow/character/" + realm + "/" + character_name + "?locale=en_US&apikey=pmqd9mhcmpek2zfdfnu975ht2rbxuwa7"
+    handleRequest(characterRequestURL, characterRequestResponse)
   });
 });
